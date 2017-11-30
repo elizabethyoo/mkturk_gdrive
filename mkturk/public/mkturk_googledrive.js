d57ab4d96ce9107750f086f7bc5d2e490761e133
Elizabeth Yoo <elizabeth.yoo17@gmail.com>
2017-11-30 16:49:57 -0500

    wip add getMetadata under loadParametersfromGDrive

--- mkturk/public/mkturk_googledrive.js
+++ mkturk/public/mkturk_googledrive.js
@@ -1,12 +1,17 @@
 //================== LOAD JSON ==================//
 async function loadParametersfromGDrive(paramfile_path){
 	try{ 
-		datastring = await loadTextFilefromGDrive(paramfile_path)
+		datastring = await loadTextFilefromGDrive(paramfile_path);
 		console.log(datastring);
+		//obtain metadata of a file and parse into JSON 
+		//Q: does a GET request handle both file download and metadata?
+		//get method description: Gets a file's metadata or content by ID.
 
 		//filemeta = await dbx.filesGetMetadata({path: paramfile_path})
+		filemeta = await downloadFile(paramfile_path);
 		//data = JSON.parse(datastring)
-
+		data = JSON.parse(datastring);
+		
 		TASK = {}
 		//TASK = data
 		/*
@@ -26,7 +31,7 @@ async function loadParametersfromGDrive(paramfile_path){
 function loadTextFilefromGDrive(textfile_path){
 	console.log("textfile_path is: " + textfile_path);
 	return new Promise(function(resolve,reject){
-	dbx.filesDownload({path: textfile_path}).then(function(data){
+	downloadFile(textfile_path).then(function(data){
 		//console.log("Read textfile "+textfile_path+" of size " + data.size)
 
 		var reader = new FileReader()
@@ -40,7 +45,7 @@ function loadTextFilefromGDrive(textfile_path){
 	console.error(error)
 	})
 	})
-	
+
 	return gapi.client.drive.files.list({
       "q": "name contains '" + textfile_path + "'"
     })
@@ -57,10 +62,11 @@ function loadTextFilefromGDrive(textfile_path){
 }
 
 
-
-  function execute() {
-    return gapi.client.drive.files.list({
-      "q": "name = '" + textfile_path + "'"
+//Identifies a file by file ID, downloads said file 
+function downloadFile(fileId) {
+	console.log(fileId);
+    return gapi.client.drive.files.get({
+      "fileId": fileId
     })
         .then(function(response) {
           // Handle the results here (response.result has the parsed body).
@@ -69,6 +75,20 @@ function loadTextFilefromGDrive(textfile_path){
           console.error("Execute error", error);
         });
   }
+
+
+//Searches Drive by file name, returns a list of matches 
+function searchFileByName() {
+	return gapi.client.drive.files.list({
+  		"q": "name = '" + textfile_path + "'"
+})
+    	.then(function(response) {
+      	// Handle the results here (response.result has the parsed body).
+      	console.log("Response", response);
+    	}, function(error) {
+      	console.error("Execute error", error);
+    });
+}
 
 
 
