7f4f371d44ca55673cd9e681580160a4e9ba302e
Elizabeth Yoo <elizabeth.yoo17@gmail.com>
2017-12-10 21:01:34 -0500

    Broken CORS

--- mkturk/public/mkturk_googledrive.js
+++ mkturk/public/mkturk_googledrive.js
@@ -1,74 +1,98 @@
 //================== LOAD JSON ==================//
 async function loadParametersfromGDrive(paramfile_path){
-	console.log("paramfile_path is: " + paramfile_path);
-	
-	return gapi.client.drive.files.list({
-  		"q": "name contains '" + paramfile_path + "'"
-	})
-    	.then(function(response) {
-      	// Handle the results here (response.result has the parsed body).
-      	console.log("Response", response);
-      	var paramfile_id = response.result.files[0].id;
-      	console.log("paramfile_id is: " + paramfile_id);
-      	console.log(response.result.files[0].id);
+	//console.log("paramfile_path is: " + paramfile_path);
 
 
-    	}, function(error) {
-      	console.error("Execute error", error);
-    });
+	var response = await searchFileByName(paramfile_path);
+	var paramfile_id = response.result.files[0].id
 
-    console.log(response.result.files[0].id);
-	
-	//var paramfile_id = searchFileByName(paramfile_path)[0]; 
-	//console.log(paramfile_id);
-	/*
 	try{ 
-		datastring = await loadTextFilefromGDrive(paramfile_path);
-		console.log("datastring is: " + datastring);
+		datastring = await loadTextFilefromGDrive(paramfile_id);
+		console.log(datastring);
 
-		//obtain metadata of a file and parse into JSON 
-		//get method description: Gets a file's metadata or content by ID.
-		//filemeta = await dbx.filesGetMetadata({path: paramfile_path})
-		filemeta = await downloadFile(paramfile_path);
-		console.log("filemeta is: " + filemeta);
-		data = JSON.parse(datastring);
-		//console.log("data is: " + data);
+		filemeta =  await downloadFile();
+		console.log(filemeta);
+
+		data = JSON.parse(datastring)
 
 		TASK = {}
-		
 		TASK = data
-		
+
 		ENV.ParamFileName = filemeta.path_display; 
 		ENV.ParamFileRev = filemeta.rev
 		ENV.ParamFileDate = new Date(filemeta.client_modified)
-	
-		
 		return 0; //need2loadParameters
 	}
+	
 	catch(error){
 		console.error('loadParametersfromGDrive() error: ' + error)
 		return 1; //need2loadParameters
 	}
-	*/
+	
 
+	//Load params file from mkturk_gitlab directory 
+	var xhr = new XMLHttpRequest();
+	
+	xhr.open("GET", "http://localhost:8000/Eliaso_params.txt");
+
+	xhr.onload = function()  {
+		var data = JSON.parse(xhr.responseText);
+		console.log(data);
+
+		TASK = {}
+		TASK = data
+
+		ENV.ParamFileName = filemeta.path_display; 
+		ENV.ParamFileRev = filemeta.rev
+		ENV.ParamFileDate = new Date(filemeta.client_modified)
+		return 0; //need2loadParameters
+	}
+
+
+	
+	xhr.send();
+
+
+
+	}
+
+
+
+function jsonp_callback(data) {
+	console.log(data);
 }
-
 function loadTextFilefromGDrive(textfile_path){
-	console.log("textfile_path is: " + textfile_path);
-	//convert textfile_path to fileId
-	//var textfile_id = searchFileByName(textfile_path);
-	//console.log("textfile_id is: " + textfile_id);
-
+	console.log("textfile_id is: " + textfile_path);
+	
 	return new Promise(function(resolve,reject){
-	downloadFile(textfile_id).then(function(data){
-
-		var reader = new FileReader()
+	downloadFile(textfile_path).then(function(data){
+		console.log(textfile_path);
+		console.log(data);
+		/*var reader = new FileReader()
 		reader.onload = function(e){
 			var data = JSON.parse(reader.result)
-			console.log("data is: " + data);
+			
 			resolve(reader.result)
-		}
-		reader.readAsText(data.fileBlob)
+		}*/
+		console.log(data.result.webContentLink);
+		$.ajax({
+		  crossDomain: true,
+		  url: data.result.webContentLink,
+		  headers: {
+		  	//'Access-Control-Allow-Credentials': true
+			'Access-Control-Allow-Origin': '*'
+			//'Access-Control-Allow-Methods': 'GET'
+			//Access-Control-Allow-Headers: Authorization
+		  },
+		  success: function(ret_data) {
+		  	console.log("Success ?");
+		  	console.log(ret_data);
+		  },
+		  error: function(ret_data) {
+		  	console.log("Failure");
+		  }
+		});
+
 	})
 .catch(function(error){
 	console.error(error)
@@ -78,33 +102,37 @@ function loadTextFilefromGDrive(textfile_path){
 	
 }
 
-
 //Downloads file whose fileId is provided 
 function downloadFile(fileId) {
 	console.log(fileId);
     return gapi.client.drive.files.get({
-      "fileId": fileId
+      "fileId": fileId,
+      "fields": "*"
     })
-        .then(function(response) {
-          // Handle the results here (response.result has the parsed body).
-          console.log("Response", response);
-        }, function(error) {
-          console.error("Execute error", error);
-        });
   }
 
+/*
+.then(function(response) {
+          // Handle the results here (response.result has the parsed body).
+          console.log("Response", response);
+          return response 
+        }
+*/
 
 //Searches Drive by file name, returns a list of matches 
 function searchFileByName(name) {
 	return gapi.client.drive.files.list({
   		"q": "name contains '" + name + "'"
 })
+	
     	.then(function(response) {
       	// Handle the results here (response.result has the parsed body).
       	console.log("Response", response);
+      	return response
     	}, function(error) {
       	console.error("Execute error", error);
     });
+   
 }
 
 
